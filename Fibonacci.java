904c39067d700563807cbf7e198d2ac0d3591fb4
Marzia <marziaafsana1013@gmail.com>
2025-07-25 22:32:21 +0600

    Add files via upload

---
+++ Fibonacci.java
@@ -0,0 +1,16 @@
+public class Fibonacci {
+    public static void main(String[] args) {
+        int n = 20;  // number of Fibonacci terms
+        int a = 0, b = 1;
+
+        System.out.print("Fibonacci series: ");
+
+        for (int i = 1; i <= n; i++) {
+            System.out.print(a + " ");
+
+            int next = a + b;
+            a = b;
+            b = next;
+        }
+    }
+}
