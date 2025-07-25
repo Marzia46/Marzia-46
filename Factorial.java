aa5aeaae89adda7d6c4cd9dcf10b30c48eaabc5d
Marzia <marziaafsana1013@gmail.com>
2025-07-25 22:23:07 +0600

    Add files via upload

---
+++ Factorial.java
@@ -0,0 +1,9 @@
+public class Factorial {
+    public static void main(String[] args) {
+        int factorial = 1;
+        for (int i = 1; i <= 5; i++) {
+            factorial = factorial * i;
+        }
+        System.out.println("Factorial of 5 is: " + factorial);
+    }
+}
